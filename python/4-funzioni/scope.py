1a5d72983f988088defe4b94f722760c5ddc96e6
aiman-al-masoud <luxlunarislabs@gmail.com>
2022-07-19 16:02:22 +0200

    ...

---
+++ python/4-funzioni/scope.py
@@ -0,0 +1,6 @@
+
+# variabili locali
+# variabili globali
+# keyword global
+# modificare globali da funzione
+# pass by val vs pass "by ref": ovvero quando i parametri vengono modificati da funzione
