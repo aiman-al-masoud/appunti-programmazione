6fbecbdf1a313d6ea70b7b68dbedfc2909ad904f
aiman-al-masoud <luxlunarislabs@gmail.com>
2022-07-19 17:26:04 +0200

    ...

--- python/1-tipi-dati/1-liste.py
+++ python/1-tipi-dati/1-liste.py
@@ -4,23 +4,31 @@
 # di elementi eterogenei.
 #
 
-l1  = [1, 2.2, "ciao", True]
+l1  = [1, 2.2, "ciao", True, 1+1j]
 print("(0)", l1)
 
-# Indicizzazione
+# INDICIZZAZIONE
 
 x = l1[0]
 y = l1[1]
 print("(1)", x, y)
 
-# Indicizzazione negativa
+# INDICIZZAZIONE NEGATIVA
 
 x = l1[-1] # ultimo elemento
 y = l1[-2] # penultimo ... 
 print("(2)", x, y)
 
+# SLICING
+
+# crea copia con elemento al primo indice incluso, all'ultimo indice escluso.
+x = l1[2:4]
+print("(3)", x)
 
 
+# x[0]  ="CAPRA"
+# print(x)
+# print(l1)
 # indexing 
 # slicing 
 
