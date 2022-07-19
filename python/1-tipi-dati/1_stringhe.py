c6812d00596cb79a4a3a99acfef8be40cc480c96
aiman-al-masoud <luxlunarislabs@gmail.com>
2022-07-19 18:27:57 +0200

    ...

--- python/1-tipi-dati/1_stringhe.py
+++ python/1-tipi-dati/1_stringhe.py
@@ -1,2 +1,32 @@
+# STRINGA
+#  
+# Una stringa è una sequenza di... stringhe di lunghezza=1.
+# (Non esiste il tipo char).
+
+s = "ciao mondo!"
+
+# INDEXING & SLICING 
+
+# proprio come nelle liste
+print(s[0])
+print(s[3:7])
+print(s[1:7:2])
+
+# APICI DOPPI == APICI SINGOLI
+
+s = "ciao mondo!"
+# oppure:
+s = 'ciao mondo!'
+
+# ESCAPE CHARS
+
+'ho detto \'ciao mondo!\''
+# oppure:
+'ho detto "ciao mondo!"'
+
+
+
+
+
 # indexing 
 # slicing 
