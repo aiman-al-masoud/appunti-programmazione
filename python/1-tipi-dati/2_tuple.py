4ad2e712ab428be09be0a5e1159a02811ed10b93
aiman-al-masoud <luxlunarislabs@gmail.com>
2022-07-19 19:05:00 +0200

    ...

--- python/1-tipi-dati/2_tuple.py
+++ python/1-tipi-dati/2_tuple.py
@@ -1,7 +1,6 @@
 # TUPLA
 #
-# Una tupla è un insieme ordinato e IMMUTABILE (a differenza delle liste) 
-# di elementi potenzialmente eterogenei.
+# Una tupla è come una lista, ma è IMMUTABILE.
 #
 
 
