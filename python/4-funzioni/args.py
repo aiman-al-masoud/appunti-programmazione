f5c06d700a30dfb0605159583fdbaecfae15a32b
aiman-al-masoud <luxlunarislabs@gmail.com>
2022-07-21 00:29:31 +0200

    ...

--- python/4-funzioni/args.py
+++ python/4-funzioni/args.py
@@ -9,7 +9,7 @@
 from functools import reduce
 
 def my_sum(*args):
-    
+
     """
     Questa funzione fa la somma di tutti i suoi (illimitati)
     argomenti.
@@ -17,3 +17,11 @@ def my_sum(*args):
     return reduce(lambda x,y:x+y, args)
 
 my_sum(1,2,3,4) # 10
+
+
+
+#
+# Reduce è una funzione di ordine superiore. Prende una funzione g e una lista,
+# applica la funzione g a due elementi della lista alla volta partendo da sinistra,
+# accumulando il risultato.
+#
