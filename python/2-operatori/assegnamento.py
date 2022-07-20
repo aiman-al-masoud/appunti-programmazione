e2c0b1b5a793a1599b216228cb99754cc607c644
aiman-al-masoud <luxlunarislabs@gmail.com>
2022-07-20 11:05:13 +0200

    ...

---
+++ python/2-operatori/assegnamento.py
@@ -0,0 +1,18 @@
+y = x = "ciao mondo"
+
+
+# if x = 0:
+    # print("capra")
+
+
+if x := 0: #non va mai. Assegno 0 a x e ritorno 0, 0 falsy. 
+    print("capra")
+
+x = 11
+def countdown():
+    global x
+    x -= 1
+    return x
+
+while x := countdown():
+    print(x)
