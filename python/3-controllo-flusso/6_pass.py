ea12689813cab9f2b2f5fbac33c421415069a349
aiman-al-masoud <luxlunarislabs@gmail.com>
2022-07-20 13:13:45 +0200

    ...

---
+++ python/3-controllo-flusso/6_pass.py
@@ -0,0 +1,24 @@
+#
+# È una no-op, non fa niente. La keyword serve 
+# dato che in Python l'indentazione è sintattica e
+# richiede almeno una riga di codice nel blocco.
+#
+#
+
+def funzione():
+
+    """
+    NB: anche se non fa niente, questa funzione 
+    ritorna None comunque.
+    """
+    pass
+
+#
+# // Nei linguaggi C-like basta lasciare 
+# // il corpo vuoto.
+# 
+# void funzione(){
+#  /* do nothing */
+# }
+#
+
