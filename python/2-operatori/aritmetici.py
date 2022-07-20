d81e2134b31fee30b9b0ae75ad32cbdffaaa800e
aiman-al-masoud <luxlunarislabs@gmail.com>
2022-07-20 10:53:12 +0200

    ...

---
+++ python/2-operatori/aritmetici.py
@@ -0,0 +1,29 @@
+# aritmetici +, -, *, /, //, **, %
+
+a = 1 + 1.8 
+b = "ciao "+"mondo"
+
+a = 1 - .2
+
+a = 21*12.2
+
+"ciao "*10
+["ciao"]*10 # molti riferimenti allo stesso oggetto
+
+4/5
+
+4//5
+
+
+2**8
+
+
+10%9
+
+
+
+
+
+
+
+
