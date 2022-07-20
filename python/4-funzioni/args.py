7918a727073cdeb9413a6c244c3d699eda60c628
aiman-al-masoud <luxlunarislabs@gmail.com>
2022-07-21 00:27:16 +0200

    ...

--- python/4-funzioni/args.py
+++ python/4-funzioni/args.py
@@ -1,6 +1,19 @@
+#
+# Una funzione in Python può accettare un numero smodato di argomenti!
+# 
+# Bisogna usare la sintassi speciale con singolo asterisco.
+# La funzione riceverà tutti gli argomenti in ordine in una singola tupla.
+#
+#
+
 from functools import reduce
 
 def my_sum(*args):
+    
+    """
+    Questa funzione fa la somma di tutti i suoi (illimitati)
+    argomenti.
+    """
     return reduce(lambda x,y:x+y, args)
 
 my_sum(1,2,3,4) # 10
