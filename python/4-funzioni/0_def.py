30d045a9e0981260276734f091302c713c41551d
aiman-al-masoud <luxlunarislabs@gmail.com>
2022-07-20 16:54:37 +0200

    ...

---
+++ python/4-funzioni/0_def.py
@@ -0,0 +1,29 @@
+# Funzioni
+#
+# Una funzione è un pezzo di codice riutilizzabile
+# che può opzionalmente prendere argomenti, e ritornare 
+# valori.
+#
+#
+
+# funzione che non fa niente, ma ritorna None (default)
+def func():
+    pass
+
+x = func()
+print(x, "")
+
+# funzione che ritorna un valore
+def fib(n):
+    """
+    Implementazione ricorsiva di fibonacci
+    """
+    if n
+
+
+# funzione che ritorna più valori!
+
+
+
+
+# documentation comment
