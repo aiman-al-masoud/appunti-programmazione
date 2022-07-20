0e9828c625c18779704037200b882cdd45c23aba
aiman-al-masoud <luxlunarislabs@gmail.com>
2022-07-20 18:28:14 +0200

    ...

--- python/4-funzioni/3_first_class.py
+++ python/4-funzioni/3_first_class.py
@@ -1,7 +1,11 @@
-#
-#
-#
-#
+# Funzioni di "prima classe" (first class)
+# 
+# In Python le funzioni sono a tutti gli effetti
+# degli oggetti, si possono assegnare a variabili
+# e passare ad altre funzioni come argomenti. 
+# 
+# Si dice che le funzioni sono "cittadini 
+# di prima classe".
 #
 #
 #
