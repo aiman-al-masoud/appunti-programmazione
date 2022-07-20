90e1971f5795a44816e4bde753baf3b0db500a2f
aiman-al-masoud <luxlunarislabs@gmail.com>
2022-07-20 13:25:35 +0200

    ...

--- python/1-tipi-dati/0_liste.py
+++ python/1-tipi-dati/0_liste.py
@@ -28,6 +28,8 @@ print("(3)", x)
 x = l1[:] 
 x[0] = 1200102 # modifico solo copia
 print("(4)", x, l1)
+
+# Shallow Copy:
 # questo metodo crea una nuova lista. Ma i riferimenti degli elementi sono gli 
 # stessi degli oggetti originali. Quindi riassegnare un un indice non modifica
 # ambo le liste. Ma modificare un oggetto lo fa.
