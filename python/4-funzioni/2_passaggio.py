4d04c67946cab8f535ab069e0c6cc30a095e5016
aiman-al-masoud <luxlunarislabs@gmail.com>
2022-07-20 17:27:30 +0200

    ...

--- python/4-funzioni/2_passaggio.py
+++ python/4-funzioni/2_passaggio.py
@@ -1,9 +1,11 @@
 #
 # Passaggio By Value vs By Reference 
 # 
-# By Value: la funzione riceve una copia 
+# By Value: la funzione riceve una copia dell'oggetto
+# che le si passa.
 #
-#
+# By Reference: la funzione riceve un riferimento
+# all'oggetto originale che le si passa.
 #
 #
 
