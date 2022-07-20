7c0db6144a7bb3e25afe7ca6d32293b496665aea
aiman-al-masoud <luxlunarislabs@gmail.com>
2022-07-21 00:14:33 +0200

    ...

--- python/2-operatori/10_spread.py
+++ python/2-operatori/10_spread.py
@@ -1,4 +1,10 @@
 # https://how.wtf/spread-operator-in-python.html
+#
+# Per svolgere lo 'spacchettamento' (unpacking) di un iterabile
+# (lista, dizionario ...).
+#
+#
+
 
 
 d = {"gatto":"miao", "cane":"bau"}
@@ -10,3 +16,9 @@ print(d)
 l = [1,2,3]
 l = [*l, 4, 5, 6]
 print(l)
+
+
+def tre_arg(a,b,c):
+    return a+b+c
+
+print(tre_arg(*[1,2,3]))
