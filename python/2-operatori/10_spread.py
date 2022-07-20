e227152687075b4db028d62a5bf238f849c54036
aiman-al-masoud <luxlunarislabs@gmail.com>
2022-07-21 00:16:22 +0200

    ...

--- python/2-operatori/10_spread.py
+++ python/2-operatori/10_spread.py
@@ -3,22 +3,25 @@
 # Per svolgere lo 'spacchettamento' (unpacking) di un iterabile
 # (lista, dizionario ...).
 #
+# Spacchettamento vuol dire potere usare gli elementi come argomenti
+# di una funzione, oppure come literals in una nuova lista/dizionario.
 #
 
+# Lista
+l = [1,2,3]
+l = [*l, 4, 5, 6]
 
+print(l)
 
+# Dizionario
 d = {"gatto":"miao", "cane":"bau"}
 
 d = {**d, "tigre":"ciuff"}
 
 print(d)
 
-l = [1,2,3]
-l = [*l, 4, 5, 6]
-print(l)
-
-
+# Usare elementi lista come argomenti
 def tre_arg(a,b,c):
     return a+b+c
 
-print(tre_arg(*[1,2,3]))
+tre_arg(*[1,2,3]) # 6
