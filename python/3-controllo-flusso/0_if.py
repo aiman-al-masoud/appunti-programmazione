8ad06943bec675f9c51a9247b6332c24ded3021f
aiman-al-masoud <luxlunarislabs@gmail.com>
2022-07-20 12:23:12 +0200

    ...

---
+++ python/3-controllo-flusso/0_if.py
@@ -0,0 +1,15 @@
+condizione = True
+altra_condizione = True
+altra_condizione2 = True
+
+# compi una sola di queste azioni, 
+# la prima per cui la condizione vale:
+
+if condizione:
+    pass # fai qualcosa
+elif altra_condizione:
+    pass # fai qualcos'altro 1
+elif altra_condizione2:
+    pass # fai qualcos'altro 2
+else: 
+    pass # se tutto il resto fallisce, fai questo 
