a69264e8288cc29f288327cbd33f34ddca96b846
aiman-al-masoud <luxlunarislabs@gmail.com>
2022-07-20 12:13:05 +0200

    ...

---
+++ python/2-operatori/9_ternario.py
@@ -0,0 +1,20 @@
+
+
+eta = 25
+x = "maggiorenne" if eta >= 18 else "minorenne"
+x # "maggiorenne"
+
+# MOLTO meglio che:
+
+if eta >= 18:
+    x = "maggiorenne"
+else:
+    x = "minorenne"
+
+
+# NB: se devi assegnare valori booleani, allora
+# basta usare gli operatori 
+
+
+
+
