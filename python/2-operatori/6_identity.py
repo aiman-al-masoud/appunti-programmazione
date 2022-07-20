6ef0518f46787fd02ca1c75d0c0638d7ff2e9cbc
aiman-al-masoud <luxlunarislabs@gmail.com>
2022-07-20 11:46:04 +0200

    ...

---
+++ python/2-operatori/6_identity.py
@@ -0,0 +1,14 @@
+#
+# Identità != Uguaglianza.
+#
+# Identità fra due variabili vuol dire che puntano
+# allo stesso oggetto in memoria.
+# 
+# Uguaglianza vuol dire che gli oggetti hanno
+# proprietà equivalenti.
+#
+# Identità implica Uguaglianza.
+# Ma non viceversa.
+#
+
+
