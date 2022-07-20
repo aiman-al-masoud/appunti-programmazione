9e16594c3d6b11b604a111c5f57d3e71761c0083
aiman-al-masoud <luxlunarislabs@gmail.com>
2022-07-20 12:28:39 +0200

    ...

---
+++ python/3-controllo-flusso/1_for.py
@@ -0,0 +1,26 @@
+
+for i in [1,2,3]:
+    print(i)
+
+
+d = {"gatto":"miao", "cane":"bau"}
+for key, val in d.items():
+    print(key, val)
+
+
+for c in "ciao mondo!":
+    print(c)
+
+
+# per emulare il for tradizionale del C:
+
+for i in range(0, 100): # for(i=0;i<100;i++)
+    print(i)
+
+
+
+# molto spesso conviene sostituire il for con una 
+# list comprehension
+
+
+
