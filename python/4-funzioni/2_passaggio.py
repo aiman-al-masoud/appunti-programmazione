ee699c1e399dbd10558ff747076a400737c8a726
aiman-al-masoud <luxlunarislabs@gmail.com>
2022-07-20 17:44:24 +0200

    ...

--- python/4-funzioni/2_passaggio.py
+++ python/4-funzioni/2_passaggio.py
@@ -11,8 +11,10 @@
 
 
 # Riassegnare un argomento funzionera solo sulla 
-# copia locale.
+# copia locale:
+
 x = 10
+
 def foo(x):
     x = 11
 
@@ -20,20 +22,18 @@ foo(x)
 print(x)
 
 
-# Invece, se il tipo è mutabile e lo si modifica senza
-# riassegnarlo, si sta modificando l'originale
+# Invece, se il tipo dell'argomento è mutabile e lo si 
+# modifica senza riassegnarlo, si sta modificando 
+# l'originale:
 
 li = [1,2]
 
 def bar(li):
     li.append(3)
     
-    # li = li + [4]
-    # li+=[4]
+    # li = li + [4] # non modifica arg, perché riassegnamento
+    # li+=[4] # strano comportamento! https://stackoverflow.com/questions/2347265/why-does-behave-unexpectedly-on-lists
 
 bar(li)
 print(li)
 
-
-
-# pass by val vs pass "by ref": ovvero quando i parametri vengono modificati da funzione
