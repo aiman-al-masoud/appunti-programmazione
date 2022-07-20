52166e32d10cdcaa468aacd227511ec8c1bf85a8
aiman-al-masoud <luxlunarislabs@gmail.com>
2022-07-20 10:54:38 +0200

    ...

--- python/2-operatori/aritmetici.py
+++ python/2-operatori/aritmetici.py
@@ -1,23 +1,26 @@
-# aritmetici +, -, *, /, //, **, %
-
+# SOMMA
 a = 1 + 1.8 
 b = "ciao "+"mondo"
 
+# SOTTRAZIONE
 a = 1 - .2
 
+# MOLTIPLICAZIONE
 a = 21*12.2
 
 "ciao "*10
 ["ciao"]*10 # molti riferimenti allo stesso oggetto
 
+# DIVISIONE
 4/5
 
+# DIVISIONE INTERI
 4//5
 
-
+# POTENZA
 2**8
 
-
+# MODULO
 10%9
 
 
