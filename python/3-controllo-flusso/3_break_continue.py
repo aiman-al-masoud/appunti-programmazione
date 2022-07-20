0ea5258e02306c4a9640478a46a44ac668e864cd
aiman-al-masoud <luxlunarislabs@gmail.com>
2022-07-20 12:57:38 +0200

    ...

--- python/3-controllo-flusso/3_break_continue.py
+++ python/3-controllo-flusso/3_break_continue.py
@@ -4,4 +4,17 @@
 #
 
 
+# es: 
 
+db = [{"nome":"Tizio", "eta":30},
+      {}, # vuoto
+      {"nome":"Caio", "eta":40},
+      {"nome":"Sempronio", "eta":50}]
+
+
+for r in db:
+    
+    if not r: # è vuoto
+        continue
+
+    print(r)
