3f3cf8c90575f08b3984a9f975b4060648526ef1
aiman-al-masoud <luxlunarislabs@gmail.com>
2022-07-20 18:18:56 +0200

    ...

--- python/4-funzioni/1_scope.py
+++ python/4-funzioni/1_scope.py
@@ -61,3 +61,6 @@ print(x)
 
 # globals(): ritorna dizionario di oggetti nello scope globale.
 # locals(): ritorna dizionario di oggetti nello scope locale (di una funzione).
+# 
+# A livello di modulo, globals() == locals()
+#
