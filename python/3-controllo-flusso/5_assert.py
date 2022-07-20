52cfca44b1eb320ac16b22929e9f850a33795182
aiman-al-masoud <luxlunarislabs@gmail.com>
2022-07-20 13:03:47 +0200

    ...

---
+++ python/3-controllo-flusso/5_assert.py
@@ -0,0 +1,13 @@
+#
+# È un modo forte per controllare che una certa 
+# condizione sia sempre rispettata ad un certo punto nel 
+# programma.
+#
+# Lancia un'eccezione se la condizione non è 
+# rispettata.
+#
+# Può fungere da documentazione e aiutare nel 
+# debugging di un programma.
+#
+
+
