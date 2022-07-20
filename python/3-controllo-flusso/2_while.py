e3fde3929e2e7ee653d977b244d15251bd655f73
aiman-al-masoud <luxlunarislabs@gmail.com>
2022-07-20 12:42:41 +0200

    ...

--- python/3-controllo-flusso/2_while.py
+++ python/3-controllo-flusso/2_while.py
@@ -1,6 +1,11 @@
+#
+# Si usa quando si è incerti di quanto potrebbe
+# durare il ciclo. Controllo ad ogni iterazione 
+# se la condizione per continuare è ancora valida.
+#
+
+
 condizione = True 
-
-
 
 while condizione:
     pass # fai qualcosa
