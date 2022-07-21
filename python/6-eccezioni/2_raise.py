ac7d0b9cde300f30ee7bbc63f028d15f0948cf0e
aiman-al-masoud <luxlunarislabs@gmail.com>
2022-07-21 15:50:40 +0200

    ...

--- python/6-eccezioni/2_raise.py
+++ python/6-eccezioni/2_raise.py
@@ -6,6 +6,30 @@
 # da dove è scaturita l'anomalia.
 #
 
+from random import choice
 
-raise Exception("ciao")
+
+def servizio_web():
+
+    """
+    Questa è una funzione non interagisce con l'utente
+    """
+
+    connessione = choice([True, False])
+    if not connessione:
+        raise ConnectionError("Connessione assente, perché errore ...")
+    
+
+def main():
+
+    """
+    L'utente interagisce con questa funzione.
+    """
+
+    try:
+        x = servizio_web()
+        # fai qualcos'altro ...
+    except :
+        print("La ")
+
 
