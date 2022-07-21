03eb0cc01d71ec2cfa92de668d95f013abc89ffd
aiman-al-masoud <luxlunarislabs@gmail.com>
2022-07-21 10:17:44 +0200

    ...

--- python/5-io/2_argparse_module.py
+++ python/5-io/2_argparse_module.py
@@ -1,5 +1,11 @@
 #
 # Ma quando ci sono veramente tanti argomenti da linea 
-# di comando, vorremmo poter
+# di comando, si ripresenta il problema degli argomenti
+# posizionali! Vorremmo poter passare gli argomenti
+# in qualsiasi ordine, come facciamo con i kwargs in Python. 
+#
+# Usando sys.argv, questo richiederebbe molto lavoro
+# da parte nostra. Fortunatamente in Python c'è già 
+# una libreria standard che fa a caso nostro.
 #
 #
