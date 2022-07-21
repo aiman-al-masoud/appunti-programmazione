317e9fe1ca08561bb9e568d0e4229f55a80c9ed1
aiman-al-masoud <luxlunarislabs@gmail.com>
2022-07-21 15:15:04 +0200

    ...

---
+++ python/6-eccezioni/try_except.py
@@ -0,0 +1,24 @@
+#
+# Un’eccezione è un evento anomalo che provoca 
+# l’interruzione del normale flusso esecuzione 
+# di un programma
+# 
+# Se un'eccezione non viene catturata, il programma uscira dall'attuale
+# blocco di codice, verso quello esterno. Se non viene catturata in nessun
+# blocco di codice estero, l'eccezione finirà per uscire il programma
+# con un errore.
+#
+# Se sospettiamo che del codice lancerà un'eccezione, possiamo 
+# gestirla tramite un try-except:
+#
+
+try: # viene eseguito tutto solo se non solleva eccezioni
+    1/0
+except Exception as e: #viene eseguito solo in caso di eccezione
+    print(e)
+
+
+
+
+
+
