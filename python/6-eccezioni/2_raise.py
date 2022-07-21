a467ee8874dc1db72623afb64fe2c32d4208ea26
aiman-al-masoud <luxlunarislabs@gmail.com>
2022-07-21 15:40:25 +0200

    ...

---
+++ python/6-eccezioni/2_raise.py
@@ -0,0 +1,11 @@
+#
+# Le eccezioni possono anche essere sollevante.
+#
+# Questo permette di delegare la gestione della situazione anomala
+# ad altre parti del programma, non necessariamente la posizione
+# da dove è scaturita l'anomalia.
+#
+
+
+raise Exception("ciao")
+
