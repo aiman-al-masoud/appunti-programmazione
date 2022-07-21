4681a8fd53df98d777a9b0b2c66806072c57aafd
aiman-al-masoud <luxlunarislabs@gmail.com>
2022-07-21 10:07:10 +0200

    ...

---
+++ python/5-io/input.py
@@ -0,0 +1,20 @@
+#
+# Ottenere un input testuale dall'utente durante 
+# l'esecuzione del programma è molto semplice in Python.
+#
+
+# il programma va in pausa, finché l'utente non preme invio.
+x = input()
+print(x)
+
+
+# si può anche opzionalmente specificare un messaggio
+# direttamente nella funzione input()
+
+x = input("inserisci il tuo nome:\n")
+print(x)
+
+# Nota che il valore di ritorno di input() è sempre
+# una stringa! Se ci si aspetta un valore numerico
+# dall'utente, bisognerà poi fare il casting esplicito.BlockingIOError()
+
