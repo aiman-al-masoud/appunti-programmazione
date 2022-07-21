fe7ca2c10ba59824d8de33bbc562d57d75ffa504
aiman-al-masoud <luxlunarislabs@gmail.com>
2022-07-21 09:48:15 +0200

    ...

--- python/4-funzioni/args.py
+++ python/4-funzioni/args.py
@@ -1,7 +1,11 @@
 #
-# Una funzione in Python può accettare un numero smodato di argomenti!
+# A volte non sappiamo a priori quanti argomenti dovremo
+# passare ad una funzione. Una valida opzione è passare una lista 
+# o tupla di argomenti. Un'altra opzione, in Python, è di passare un numero 
+# smodato di argomenti!
 # 
 # Bisogna usare la sintassi speciale con singolo asterisco.
+#
 # La funzione riceverà tutti gli argomenti in ordine in una singola tupla.
 #
 #
