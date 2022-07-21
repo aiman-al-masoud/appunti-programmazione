b20e217d5e65e9a5ebcfff7135ef1a85bc2f36fd
aiman-al-masoud <luxlunarislabs@gmail.com>
2022-07-21 10:13:32 +0200

    ...

--- python/5-io/1_arg_cli.py
+++ python/5-io/1_arg_cli.py
@@ -1,7 +1,6 @@
 #
 # Spesso risulterà più comodo passare degli argomenti
-# ad uno script quando viene lanciato, perché è più
-# facile da integrare con altri programmi.
+# ad uno script quando viene lanciato.
 # 
 # Il modo più semplice di leggere argomenti da linea di 
 # comando è di usare l'argv del modulo sys.
