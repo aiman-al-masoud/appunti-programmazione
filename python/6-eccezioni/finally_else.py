6caa03ba6978e835248205c46362062933963581
aiman-al-masoud <luxlunarislabs@gmail.com>
2022-07-21 15:32:32 +0200

    ...

--- python/6-eccezioni/finally_else.py
+++ python/6-eccezioni/finally_else.py
@@ -1,9 +1,11 @@
 #
+# Finally
 # La clausola finally viene eseguita sempre e comunque (sia in caso
 # di errore che non), e potrebbe servire per chiudere una risorsa 
 # aperta in precedenza, cosa che si può fare anche nell'except, 
 # o automaticamente in alcuni casi col with statement.
 #
+# Else
 # La clausola else dopo except contiene codice da eseguire solo
 # se è andato tutto bene e non ci sono state eccezioni (potrebbe anche 
 # stare direttamente nel try). 
