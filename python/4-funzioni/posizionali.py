a3997c372a52699d6bfeccfe11e8e4d8b53bbcfc
aiman-al-masoud <luxlunarislabs@gmail.com>
2022-07-21 09:46:27 +0200

    ...

--- python/4-funzioni/posizionali.py
+++ python/4-funzioni/posizionali.py
@@ -8,3 +8,21 @@ def stampa(text, ripetiz):
         print(text)
 
 stampa("ciao",3)
+
+#
+# Gli argomenti posizionali (se ci sono) sono i primi
+# che una funzione accetta, e l'ordine è importante
+#
+
+# stampa(3, "") # TypeError: 'str' object cannot be interpreted as an integer
+
+#
+# Si possono anche impostare valori di default
+# degli argomenti, così:
+#
+
+def stampa(text, ripetiz=2):
+    for i in range(ripetiz):
+        print(text)
+
+stampa("ciao mondo")
