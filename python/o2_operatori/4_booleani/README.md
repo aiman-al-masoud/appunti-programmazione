21edf1d576500bf9fdfebf2115e1c21c3e0c6740
aiman-al-masoud <luxlunarislabs@gmail.com>
2022-07-22 18:20:53 +0200

    ...

---
+++ python/o2_operatori/4_booleani/README.md
@@ -0,0 +1,3 @@
+# Booleani
+
+Già visti nella [sezione tipi dati booleani](../../o1_tipi_dati/o5_booleani/0-operatori.md).
--- python/o2_operatori/4_booleani/o4_booleani.py
+++
@@ -1,6 +0,0 @@
-import sys, os 
-sys.path.append(os.path.join(os.path.split(__file__)[0], ".."))
-
-# già visti in:
-from o1_tipi_dati import o1_stringhe
-
